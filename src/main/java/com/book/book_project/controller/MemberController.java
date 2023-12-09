551789ba901e40600551ed440b7ae9ef525864f0
JohBeomju <dpfwnl0119@gmail.com>
2023-12-09 22:49:02 +0900

    12.09-조범주-unMemberPurchaseList.html/MemberPurchaseList.html 작성

--- src/main/java/com/book/book_project/controller/MemberController.java
+++ src/main/java/com/book/book_project/controller/MemberController.java
@@ -1,9 +1,11 @@
 package com.book.book_project.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
 
     //회원 등록 화면 보기
--- src/main/java/com/book/book_project/controller/ProductController.java
+++ src/main/java/com/book/book_project/controller/ProductController.java
@@ -1,12 +1,17 @@
 package com.book.book_project.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class BoardController {
+@RequiredArgsConstructor
+public class ProductController {
 
     // main화면 보기
-    @GetMapping("/board/main")
+    @GetMapping("/product/main")
     public void getSignup() {}
+
+    @GetMapping("/product/productInfo")
+    public void getReview(){}
 }
