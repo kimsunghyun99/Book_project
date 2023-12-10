220a03dffb96ef5fd7a855e28628890d6538ab92
Leesangbaek <dltkdqor0709@naver.com>
2023-12-10 13:28:01 +0900

    김정인 / 23.12.10 / mem controller 추가

--- src/main/java/com/book/book_project/controller/MemberController.java
+++ src/main/java/com/book/book_project/controller/MemberController.java
@@ -12,6 +12,40 @@ public class MemberController {
     @GetMapping("/member/signup")
     public void getSignup() {}
 
+    //로그인 화면 보기
+    @GetMapping("/member/login")
+    private void getLogin() {}
+
+    //마이페이지 화면
+    @GetMapping("/member/mypage")
+    private void getMyPage() {}
+
+    //아이디 찾기 화면
+    @GetMapping("/member/idSearch")
+    private void getIdSearch() {}
+
+    //패스워드 찾기 화면
+    @GetMapping("/member/pwSearch")
+    private void getPwSearch() {}
+
+    //패스워드 변경 화면
+    @GetMapping("/member/pwModify")
+    private void getPwModify() {}
+
+    //회원 정보 수정 화면
+    @GetMapping("/member/memberInfoModify")
+    private void getMemberInfoModify() {}
+
+    //구매내역 조회 화면
     @GetMapping("/member/memberPurchaseList")
-    public void getMemeberPurchaseList(){}
+    private void getMemberPurchaseList() {}
+
+    //비회원 로그인 화면
+    @GetMapping("/member/unMemberLogin")
+    private void getUnMemberLogin() {}
+
+    //비회원 구매내역 조회 화면
+    @GetMapping("/member/unMemberPurchaseList")
+    private void getUnMemberPurchasesList() {}
+
 }
