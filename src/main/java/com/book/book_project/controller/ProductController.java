1e3d4480719046ac9f383363023291c2f8f25267
yunju <qlqj0101@gmail.com>
2023-12-13 09:06:06 +0900

    윤주 231213 product 작업

--- src/main/java/com/book/book_project/controller/ProductController.java
+++ src/main/java/com/book/book_project/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.book.book_project.controller;
 import com.book.book_project.dto.ProductDTO;
 import com.book.book_project.dto.ReviewInterface;
 import com.book.book_project.entity.ProductEntity;
+import com.book.book_project.service.FavoritesService;
 import com.book.book_project.service.ProductService;
 import com.book.book_project.util.PageUtil;
 import jakarta.servlet.http.HttpSession;
@@ -22,6 +23,7 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService service;
+    private final FavoritesService favoritesService;
 
     // main화면 보기
     @GetMapping("/product/main")
@@ -38,14 +40,38 @@ public class ProductController {
     }
 
     @GetMapping("/product/favoritesList")
-    public void getFavoritesList(){}
+    public void getFavoritesList(HttpSession session, Model model) throws Exception {
+        String userid = (String)session.getAttribute("userid");
+
+    }
 
     @GetMapping("/product/productList")
     public void getProductList(){}
 
     @GetMapping("/product/shoppingBasket")
     public void getShoppingBasket(){}
-}
+
 
     //댓글 처리
 
+    //리뷰 처리
+    @ResponseBody
+    @PostMapping("/product/review")
+    public List<ReviewInterface> postReview(ReviewInterface review, @RequestParam("option") String option) throws Exception {
+
+        switch (option) {
+
+            case "I":
+                service.ReviewRegistry(review); //리뷰 등록
+                break;
+            case "U":
+                service.ReviewUpdate(review); //리뷰 수정
+                break;
+            case "D":
+                service.ReviewDelete(review); //리뷰 삭제
+                break;
+        }
+
+        return service.ReviewView(review);
+    }
+}
--- src/main/java/com/book/book_project/service/ProductService.java
+++ src/main/java/com/book/book_project/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.book.book_project.service;
 
+import com.book.book_project.dto.FavoritesDTO;
 import com.book.book_project.dto.ProductDTO;
 import com.book.book_project.dto.ReviewInterface;
 import com.book.book_project.entity.ProductEntity;
@@ -16,6 +17,19 @@ public interface ProductService {
 
     //상세 내용 보기
     public ProductDTO view(int bookid) throws Exception;
+
+    //리뷰 목록 보기
+    public List<ReviewInterface> ReviewView(ReviewInterface review) throws Exception;
+
+    //리뷰 등록
+    public void ReviewRegistry(ReviewInterface review) throws Exception;
+
+    //리뷰 수정
+    public void ReviewUpdate(ReviewInterface review) throws Exception;
+
+    //리뷰 삭제
+    public void ReviewDelete(ReviewInterface review) throws Exception;
+
 
 
 
