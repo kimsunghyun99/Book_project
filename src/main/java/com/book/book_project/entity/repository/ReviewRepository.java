ff644f767b1b7442cb38764b5dea1bbdf220240e
JohRyan <dpfwnl0119@gmail.com>
2023-12-13 10:02:44 +0900

    23.12.13-수정

--- src/main/java/com/book/book_project/entity/repository/ReviewRepository.java
+++ src/main/java/com/book/book_project/entity/repository/ReviewRepository.java
@@ -12,8 +12,8 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
 @Query(value="select * from tbl_review where bookid=:bookid order by reviewseq desc", nativeQuery = true)
-	List<ReviewInterface> reviewView(@Param("bookid") Long bookid);
+	List<ReviewInterface> reviewView(@Param("bookid") int bookid);
 
 @Query(value="select * from tbl_review where bookid=:bookid order by reviewseq desc", nativeQuery = true)
-	List<ReviewDTO> reviewView1(@Param("bookid") Long bookid);
+	List<ReviewDTO> reviewView1(@Param("bookid") int bookid);
         }
