844d2ee43ee5166e4235c9ef226ad357913e4900
JohRyan <dpfwnl0119@gmail.com>
2023-12-20 19:40:55 +0900

    23.12.19-조범주 수정

--- src/main/java/com/book/book_project/service/PurchaseInfoServiceImpl.java
+++ src/main/java/com/book/book_project/service/PurchaseInfoServiceImpl.java
@@ -2,6 +2,7 @@ package com.book.book_project.service;
 
 import com.book.book_project.entity.BuyerInfoEntity;
 import com.book.book_project.entity.PurchaseInfoEntity;
+import com.book.book_project.entity.UnMemberEntity;
 import com.book.book_project.entity.repository.PurchaseInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,11 @@ public class PurchaseInfoServiceImpl implements PurchaseInfoService {
 
 
         return repository.findByBuyerseq(buyerseq);
+    }
+
+    @Override
+    public List<PurchaseInfoEntity> unMemberPurchaseList(UnMemberEntity unmembertelno) throws Exception {
+        return null;
     }
 
 }
